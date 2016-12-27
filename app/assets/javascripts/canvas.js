a4adad72294e4f91e0090419d46530dd3b296f2a
ERRECabrera <raul@mytwinplace.com>
2016-12-27 13:56:52 +0000

    size of canvas text fixed

--- app/assets/javascripts/canvas.js
+++ app/assets/javascripts/canvas.js
@@ -3,22 +3,21 @@
 function render_login_canvas(){
   var c = document.getElementsByTagName("canvas");
   var ctx = c[0].getContext("2d");
-  
+
   ctx.save();
 
   ctx.globalCompositeOperation = 'xor';
 
   var b_width = $(window).width();
   var b_height = 945;
-  
-  var font_size = 271.2;
-  ctx.font = "bold "+font_size+"pt Abel";
-  ctx.fillStyle = "#9ff";
-  ctx.fillText("LOGIN",(b_width*0.069),(b_height/3));
 
+  var font_size = 193;
+  ctx.font = "bold "+font_size+"pt Helvetica";
+  ctx.fillStyle = "#9ff";
+  ctx.fillText("LOGIN",(b_width*0.055),(b_height/3.5));
   ctx.beginPath();
-  ctx.moveTo((b_width*0.069)+25,(b_height/3)+50);
-  ctx.lineTo((b_width*0.069)+806,(b_height/3)+50);
+  ctx.moveTo((b_width*0.052)+25,(b_height/3.5)+50);
+  ctx.lineTo((b_width*0.052)+806,(b_height/3.5)+50);
   ctx.lineWidth=1;
   ctx.stroke();
 
